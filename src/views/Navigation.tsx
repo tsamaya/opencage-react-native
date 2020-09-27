7b81e9812855162e063e9a49d490c99a5cec3fe2
Arnaud Ferrand <arferrand@gmail.com>
2020-09-27 15:34:10 +0100

    add a detail screen

---
+++ src/views/Navigation.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+
+const { Navigator, Screen } = createStackNavigator();
+
+const HomeNavigator = () => (
+  <Navigator headerMode="none">
+    <Screen name="Home" component={HomeScreen} />
+    <Screen name="Details" component={DetailsScreen} />
+  </Navigator>
+);
+
+const AppNavigator = () => (
+  <NavigationContainer>
+    <HomeNavigator />
+  </NavigationContainer>
+);
+
+export default AppNavigator;
